9a70e23484bc73bae7618903c057365dba18b12c
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-07 10:21:42 +0300

    create and register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,8 @@ def create_app(config_name):
     
     
     
-    
+    #register blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
     return app
