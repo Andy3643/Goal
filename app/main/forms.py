c804eb52ea14cf5ba25482bdff63b267b626f2cd
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-10 11:56:55 +0300

    feat:upload pitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
-from wtforms import StringField,TextAreaField,SubmitField,SelectField
-from wtforms.validators import length,DataRequired
+from wtforms import TextAreaField,SubmitField,SelectField
+from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
 
@@ -7,3 +7,20 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Add some information about You')
     submit = SubmitField('Submit')
     
+class UploadPitch(FlaskForm):
+    category=SelectField('Select Category',validators=[DataRequired()],
+                         choices=[
+                            ('Interview Pitch','Interview Pitch'),
+                            ('Products Pitch','Products Pitch'),
+                            ('Tech Pitch','Tech Pitch'),
+                            ('Political Pitch','Political Pitch'),
+                            ('Religious Pitch','Religious Pitch'),
+                            ('Pickup Lines','Pickup Lines'),
+                            ('Others','None of the above')
+                            ])
+    pitch=TextAreaField('Write Pitch:',validators=[DataRequired()])
+    submit=SubmitField('Post Pitch')
+    
+class CommentsForm(FlaskForm):
+    comment=TextAreaField('Add a comment:', validators=[DataRequired()])
+    submit=SubmitField('Post Comment')
